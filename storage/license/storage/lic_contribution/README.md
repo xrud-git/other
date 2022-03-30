cfcaea4744e9ad7adcfcc05293f8203f9f036e32
irvnriir <13949704+irvnriir@users.noreply.github.com>
2022-03-30 04:11:38 +0000

    +

--- storage/license/storage/lic_contribution/README.md
+++ storage/license/storage/lic_contribution/README.md
@@ -4,3 +4,6 @@ This license for free-to-use products, is intended to unify publication of chang
 Under the license restrictions, the product can be published freely, but only if the modified parts are also contributed to the official distribution .
 
 At modified parts of the original product, it require to keep the license restrictions, but allows to add any other .
+* It can't be redistributed in compiled form, to improve disclosure and safety of it's functions .
+
+(work which : use functions, but doesn't modify them or concepts, of this work ; can be distributed under any license, even if its an addition to this work)
--- system/git_setSubmodule.cmd
+++ system/git_setSubmodule.cmd
@@ -24,19 +24,44 @@ if "%~1" equ "" (
 	cd /d "!v!"
 
 	set "v1=%~1"
+	
+	rem //# isEmpty
+	set "st=1"
+	for /f usebackq^ tokens^=*^ delims^=^ eol^= %%x in (`dir /b "!v1!"`) do (
+		set "st=0"
+	)
+	
 	call !en! removeTextFromStartInLine "v1" "!v!\"
+	
+	rem //# required by Github . [dev Git converts `//` to `/`]
 	set "v1=!v_f!"
+	set "v_s=!v_f:\=//!"
 
-	echo(Sub-repository location = !v1!
+	echo(Sub-repository location = !v_s!
 	echo(
 
 	set /p "v=Main/remote location of the target repository, or Enter for def = "||set "v=!v!"
 	echo(
 
-	git submodule add -f --depth 1 "!v!" "!v1!"
-
+	rem //# if no file/folder, or Repository is, at target path
+	git submodule add -f --depth 1 "!v!" "!v_s!"
+	
 	if "!errorLevel!" neq "0" (
-		git submodule set-url "!v1!" "!v!"
+		
+		if "!st!" equ "1" (
+			rem //# empty
+			echo(
+			echo(Clone the Repository, then run setSubmodule
+		) else (
+			call !en! checkFile "!v1!\.git"
+			if "!v_f!" equ "" (
+				echo(
+				echo(Clone the Repository, then run setSubmodule
+			) else (
+				git submodule set-url "!v_s!" "!v!"
+			)
+		)
+
 	)
 
 	echo(
